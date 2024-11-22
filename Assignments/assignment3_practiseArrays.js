a1c4b6764e44e6f30fc3fdc1c26d1321bf54f03f
anant.jain <anant.jain@SDI-HLANANTJ.local>
2024-11-22 18:30:14 +0530

    practise

---
+++ Assignments/assignment3_practiseArrays.js
@@ -0,0 +1,24 @@
+// create an array in js
+
+const cars = [];
+
+// add value in array
+
+cars.push('Maruti')
+cars.push('tata')
+cars.push('kia')
+
+console.log(cars)
+
+// create array using new array() method
+
+const bikes = new Array(['hero','honda','yamaha'])
+
+console.log(bikes)
+
+const animals = new Array()
+animals.push('dog')
+animals.push('cat')
+animals.push('goat')
+
+console.log(animals)
