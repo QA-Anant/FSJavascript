046efbf3baf7809c1470595bb66f043af72acbc0
anant.jain <anant.jain@SDI-HLANANTJ.local>
2024-12-19 08:22:00 +0530

    insertion sort

--- liveClassSessions/DSA/Sorting Algorithms/insertionSort.js
+++ liveClassSessions/DSA/Sorting Algorithms/insertionSort.js
@@ -17,17 +17,24 @@ const arr = [4,8,5,7,65,9]
 
 function findElementUsingInsertionSort(arr){
 
-    for(let i=1; i < arr.length ; i++ )
+    for(let i=1; i < arr.length ; i++ ) //<-- this will skim through the unsorted section [8,5,7,10,9]
     {
         let cv = arr[i];
         let CVNI = i; //<---- whole game is to find this right spot for the  
 
-        for(let j = i-1 ; j >= 0 ; j--)
+        for(let j = i-1 ; j >= 0 ; j--) // <-- this will skim through the sorted section [4]
         {
-            
+            if(cv < arr[j]) // element of unsorted compared to the element of sorted array
+            {
+                arr[j+1] = arr[j]
+                CVNI = j
+            }
         }
+        arr[CVNI] = cv;
 
     }
+    return arr;
 }
 
 const sortedarray = findElementUsingInsertionSort(arr)
+console.log(`Sorted array using insertion sort ${sortedarray}`)
